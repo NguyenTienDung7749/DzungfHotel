69a260c9007ced95d18861032eedab47b265c033
NguyenTienDung7749 <toinangpham@gmail.com>
2026-04-03 17:05:16 +0700

    Fix desktop full-width layout

--- includes/header.php
+++ includes/header.php
@@ -29,7 +29,7 @@ $currentUser = current_user();
     <link href="<?= e(asset('css/style.css')) ?>" rel="stylesheet">
 </head>
 <body>
-    <div class="container-xxl bg-white p-0">
+    <div class="app-shell app-container-wide bg-white p-0">
         <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
             <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
                 <span class="sr-only">Đang tải...</span>
